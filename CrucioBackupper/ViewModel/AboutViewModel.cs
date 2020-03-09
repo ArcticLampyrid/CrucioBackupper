0ba6118c6dfc7a9db0552f1e384d315b6ff8634f
qiqiworld <1354092549@qq.com>
2020-03-09 12:35:13 +0800

    Show version information on MainWindow

---
+++ CrucioBackupper/ViewModel/AboutViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CrucioBackupper.ViewModel
+{
+    public class AboutViewModel
+    {
+        public static AboutViewModel Instance { get; } = new AboutViewModel();
+
+        private AboutViewModel()
+        {
+            var versionInfo = Attribute.GetCustomAttribute(typeof(AboutViewModel).Assembly, typeof(AssemblyInformationalVersionAttribute))
+                as AssemblyInformationalVersionAttribute;
+            var informationalVersion = versionInfo?.InformationalVersion;
+            if (string.IsNullOrEmpty(informationalVersion))
+            {
+                VersionDesc = "vUnknown";
+            }
+            else
+            {
+                VersionDesc = $"v{informationalVersion}";
+            }
+        }
+
+        public string VersionDesc { get; }
+    }
+}
