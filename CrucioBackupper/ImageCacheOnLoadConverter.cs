da0ebb34f32f95d3ed4ec0e880b71963cdc18436
ArcticLampyrid <ArcticLampyrid@outlook.com>
2024-01-20 04:22:35 +0800

    fix: avoid image files locked by wpf framework

---
+++ CrucioBackupper/ImageCacheOnLoadConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace CrucioBackupper
+{
+    public class ImageCacheOnLoadConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var path = (string)value;
+            if (path == null)
+            {
+                return null;
+            }
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = new Uri(path);
+            image.EndInit();
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
