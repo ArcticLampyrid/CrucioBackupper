f496cb184830821321798ed0182c42ca7c175fdb
ArcticLampyrid <ArcticLampyrid@outlook.com>
2026-02-18 21:46:16 +0800

    perf: reuse brush for log level

--- CrucioBackupper/LogViewer/LogEventLevelToBrushConverter.cs
+++ CrucioBackupper/LogViewer/LogEventLevelToBrushConverter.cs
@@ -8,18 +8,25 @@ namespace CrucioBackupper.LogViewer
 {
     public class LogEventLevelToBrushConverter : IValueConverter
     {
+        private static readonly IBrush VerboseBrush = new SolidColorBrush(Color.FromRgb(0x66, 0x66, 0x66));
+        private static readonly IBrush DebugBrush = new SolidColorBrush(Color.FromRgb(0x6A, 0x5A, 0xCD));
+        private static readonly IBrush InformationBrush = new SolidColorBrush(Color.FromRgb(0x00, 0x64, 0x00));
+        private static readonly IBrush WarningBrush = new SolidColorBrush(Color.FromRgb(0xFF, 0xA5, 0x00));
+        private static readonly IBrush ErrorBrush = new SolidColorBrush(Color.FromRgb(0x8B, 0x00, 0x00));
+        private static readonly IBrush FatalBrush = new SolidColorBrush(Color.FromRgb(0x80, 0x00, 0x00));
+
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value is LogEventLevel level)
             {
                 return level switch
                 {
-                    LogEventLevel.Verbose => new SolidColorBrush(Color.FromRgb(0x66, 0x66, 0x66)),// Dark gray
-                    LogEventLevel.Debug => new SolidColorBrush(Color.FromRgb(0x6A, 0x5A, 0xCD)),// Slate blue
-                    LogEventLevel.Information => new SolidColorBrush(Color.FromRgb(0x00, 0x64, 0x00)),// Dark green
-                    LogEventLevel.Warning => new SolidColorBrush(Color.FromRgb(0xFF, 0xA5, 0x00)),// Orange
-                    LogEventLevel.Error => new SolidColorBrush(Color.FromRgb(0x8B, 0x00, 0x00)),// Dark red
-                    LogEventLevel.Fatal => new SolidColorBrush(Color.FromRgb(0x80, 0x00, 0x00)),// Maroon
+                    LogEventLevel.Verbose => VerboseBrush,
+                    LogEventLevel.Debug => DebugBrush,
+                    LogEventLevel.Information => InformationBrush,
+                    LogEventLevel.Warning => WarningBrush,
+                    LogEventLevel.Error => ErrorBrush,
+                    LogEventLevel.Fatal => FatalBrush,
                     _ => Brushes.Black,
                 };
             }
